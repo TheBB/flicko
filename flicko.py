8dc35277e47ced2db27cb678badc27f0f5f92cc1
Eivind Fonn <evfonn@gmail.com>
2012-11-27 18:44:34 +0100

    Test with normal-based model.

--- flicko.py
+++ flicko.py
@@ -1,66 +1,139 @@
 #!/usr/bin/python3
 
+import time
+
 from numpy import *
+from scipy.stats import norm
+from math import log
+import scipy.optimize as opt
 
 class Player:
     pass
 
-def update(player, opponents, wins, losses):
+def update(player, opps, W, L):
 
-    mu = zeros(len(opponents))
-    s = zeros(len(opponents))
-    pd = zeros(len(opponents))
-    D_delta = zeros((len(opponents), 1+len(player.cat_rating)))
-    D_delta[:,0] = 1
-    for j in range(0,len(opponents)):
-        opp = opponents[j]
-        mu[j] = (opp.rating - sqrt(3) * opp.cat_rating[player.cat]) / sqrt(6)
-        s[j] = sqrt((1/6*opp.dev**2 + 1/2*opp.cat_dev[player.cat]**2 + 1/9*pi)*3/pi**2)
-        pd[j] = opp.rating + opp.cat_rating[player.cat]
-        D_delta[j,opp.cat+1] = 1
+    mu = zeros(len(opps))
+    s = zeros(len(opps))
+    q = zeros((len(opps), len(player.cat_rating)))
+    q[:,0] = 1
+    for j in range(0,len(opps)):
+        opp = opps[j]
+        mu[j] = 2*(opp.rating + opp.cat_rating[player.cat])
+        s[j] = sqrt(1 + opp.dev**2 + opp.cat_dev[player.cat]**2)
+        if opp.cat < len(player.cat_rating) - 1:
+            q[j,opp.cat+1] = 1
+        else:
+            q[j,1:] = -1
 
-    delta = lambda x,j: x[0] + x[opponents[j].cat+1] - pd[j]
-    l = lambda x,j: 1/(1 + exp(-(delta(x,j)+mu[j])/s[j]))
-    w = lambda x,j: 1/(1 + exp( (delta(x,j)+mu[j])/s[j]))
-    g = lambda y: y**2 * 1/(1/y-1)
+    def make_cats(x):
+        cats = zeros(len(x))
+        cats[:-1] = x[1:]
+        cats[-1] = 1 - sum(x)
+        return cats
 
-    def Dlog(x):
-        ret = zeros(1+len(player.cat_rating))
-        for j in range(0,len(opponents)):
-            ret += (losses[j]*l(x,j) - wins[j]*w(x,j)) * D_delta[j,:] / s[j]
-        return ret
+    def logL(x):
+        ret = 0
+        cats = make_cats(x)
+        for j in range(0,len(opps)):
+            t = x[0] + cats[opps[j].cat]
+            Phi = norm.cdf(t, loc=mu[j], scale=s[j])
+            ret += W[j]*log(Phi) + L[j]*log(1-Phi)
+        return -ret
 
-    def D2log(x):
-        ret = zeros((1+len(player.cat_rating), 1+len(player.cat_rating)))
-        for j in range(0,len(opponents)):
-            ret += (losses[j]*g(l(x,j)) - wins[j]*g(w(x,j))) *\
-                    outer(D_delta[j,:],D_delta[j,:]) / s[j]**2
-        return ret
+    def DlogL(x):
+        ret = zeros(len(x))
+        cats = make_cats(x)
+        for j in range(0,len(opps)):
+            t = x[0] + cats[opps[j].cat]
+            phi = norm.pdf(t, loc=mu[j], scale=s[j])
+            Phi = norm.cdf(t, loc=mu[j], scale=s[j])
+            ret += (W[j]/Phi - L[j]/(1-Phi))*phi * q[j,:]
+        return -ret
 
-    x = zeros(1+len(player.cat_rating))
+    def D2logL(x):
+        ret = zeros((len(x),len(x)))
+        cats = make_cats(x)
+        for j in range(0,len(opps)):
+            t = x[0] + cats[opps[j].cat]
+            phi = norm.pdf(t, loc=mu[j], scale=s[j])
+            Phi = norm.cdf(t, loc=mu[j], scale=s[j])
+            val = -(W[j]/Phi**2 + L[j]/(1-Phi)**2) * phi**2
+            val -= (W[j]/Phi - L[j]/(1-Phi))*phi*(t-mu[j])/s[j]**2
+            ret += val * outer(q[j,:], q[j,:])
+        return -ret
+
+    x = zeros(len(player.cat_rating))
     x[0] = player.rating
-    for c in range(0,len(player.cat_rating)):
-        x[c+1] = player.cat_rating[c]
+    for c in range(0,len(player.cat_rating)-1):
+        x[1+c] = player.cat_rating[c]
 
-    for i in range(0,5):
-        D = Dlog(x)
-        D2 = D2log(x)
-        (q,residues,rank,s) = linalg.lstsq(D2,D)
-        x = x - 1e-2*q
-        print(x)
+    if False:
+        start = time.clock()
+        for i in range(0,100):
+            opt.fmin(logL, x, disp=False)
+        end = time.clock()
+        print('Downhill simplex: {t:.2f} seconds.'.format(t=end-start))
+
+        start = time.clock()
+        for i in range(0,100):
+            opt.fmin_powell(logL, x, disp=False)
+        end = time.clock()
+        print('Powell: {t:.2f} seconds.'.format(t=end-start))
+
+        start = time.clock()
+        for i in range(0,100):
+            opt.fmin_cg(logL, x, fprime=DlogL, disp=False)
+        end = time.clock()
+        print('CG: {t:.2f} seconds.'.format(t=end-start))
+
+        start = time.clock()
+        for i in range(0,100):
+            opt.fmin_bfgs(logL, x, fprime=DlogL, disp=False)
+        end = time.clock()
+        print('BFGS: {t:.2f} seconds.'.format(t=end-start))
+
+        start = time.clock()
+        for i in range(0,100):
+            opt.fmin_ncg(logL, x, fprime=DlogL, fhess=D2logL, disp=False)
+        end = time.clock()
+        print('Newton-CG: {t:.2f} seconds.'.format(t=end-start))
+
+    x = opt.fmin_ncg(logL, x, fprime=DlogL, fhess=D2logL, disp=True)
+    print(x)
+    print(D2logL(x))
+    print(linalg.det(D2logL(x)))
 
 p = Player()
 p.rating = 0
-p.dev = 10
 p.cat_rating = [0, 0, 0]
-p.cat_dev = [10, 10, 10]
+p.dev = 1
+p.cat_dev = [1, 1, 1]
 p.cat = 0
+
+opps = []
 
 q = Player()
 q.rating = 0
-q.dev = 10
 q.cat_rating = [0, 0, 0]
-q.cat_dev = [10, 10, 10]
+q.dev = 1
+q.cat_dev = [1, 1, 1]
 q.cat = 0
+opps += [q]
 
-update(p, [q], [3], [1])
+q = Player()
+q.rating = 5
+q.cat_rating = [0, 0, 0]
+q.dev = 1
+q.cat_dev = [1, 1, 1]
+q.cat = 1
+opps += [q]
+
+q = Player()
+q.rating = 10
+q.cat_rating = [0, 0, 0]
+q.dev = 1
+q.cat_dev = [1, 1, 1]
+q.cat = 1
+opps += [q]
+
+update(p, opps, [3, 2, 0], [1, 4, 4])
