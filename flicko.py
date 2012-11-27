76e6ee26d950ccf594e37fb322a7a1b7e7c7b98f
Eivind Fonn <evfonn@gmail.com>
2012-11-27 23:16:00 +0100

    ...

--- flicko.py
+++ flicko.py
@@ -12,30 +12,37 @@ class Player:
 
 def update(player, opps, W, L):
 
+    tot_catrating = 0
+    categories = []
+    for j in range(0,len(opps)):
+        if opps[j].cat not in categories:
+            categories.append(opps[j].cat)
+            tot_catrating += player.cat_rating[opps[j].cat]
+
     mu = zeros(len(opps))
     s = zeros(len(opps))
-    q = zeros((len(opps), len(player.cat_rating)))
+    q = zeros((len(opps), len(categories)))
     q[:,0] = 1
     for j in range(0,len(opps)):
         opp = opps[j]
         mu[j] = 2*(opp.rating + opp.cat_rating[player.cat])
         s[j] = sqrt(1 + opp.dev**2 + opp.cat_dev[player.cat]**2)
         if opp.cat < len(player.cat_rating) - 1:
-            q[j,opp.cat+1] = 1
+            q[j,categories.index(opp.cat)] = 1
         else:
             q[j,1:] = -1
 
     def make_cats(x):
         cats = zeros(len(x))
         cats[:-1] = x[1:]
-        cats[-1] = 1 - sum(x)
+        cats[-1] = tot_catrating - sum(x[1:])
         return cats
 
     def logL(x):
         ret = 0
         cats = make_cats(x)
         for j in range(0,len(opps)):
-            t = x[0] + cats[opps[j].cat]
+            t = x[0] + cats[categories.index(opps[j].cat)]
             Phi = norm.cdf(t, loc=mu[j], scale=s[j])
             ret += W[j]*log(Phi) + L[j]*log(1-Phi)
         return -ret
@@ -44,7 +51,7 @@ def update(player, opps, W, L):
         ret = zeros(len(x))
         cats = make_cats(x)
         for j in range(0,len(opps)):
-            t = x[0] + cats[opps[j].cat]
+            t = x[0] + cats[categories.index(opps[j].cat)]
             phi = norm.pdf(t, loc=mu[j], scale=s[j])
             Phi = norm.cdf(t, loc=mu[j], scale=s[j])
             ret += (W[j]/Phi - L[j]/(1-Phi))*phi * q[j,:]
@@ -54,7 +61,7 @@ def update(player, opps, W, L):
         ret = zeros((len(x),len(x)))
         cats = make_cats(x)
         for j in range(0,len(opps)):
-            t = x[0] + cats[opps[j].cat]
+            t = x[0] + cats[categories.index(opps[j].cat)]
             phi = norm.pdf(t, loc=mu[j], scale=s[j])
             Phi = norm.cdf(t, loc=mu[j], scale=s[j])
             val = -(W[j]/Phi**2 + L[j]/(1-Phi)**2) * phi**2
@@ -62,48 +69,20 @@ def update(player, opps, W, L):
             ret += val * outer(q[j,:], q[j,:])
         return -ret
 
-    x = zeros(len(player.cat_rating))
+    x = zeros(len(categories))
     x[0] = player.rating
-    for c in range(0,len(player.cat_rating)-1):
-        x[1+c] = player.cat_rating[c]
+    for c in range(0,len(categories)-1):
+        x[1+c] = player.cat_rating[categories[c]]
 
-    if False:
-        start = time.clock()
-        for i in range(0,100):
-            opt.fmin(logL, x, disp=False)
-        end = time.clock()
-        print('Downhill simplex: {t:.2f} seconds.'.format(t=end-start))
+    ret = opt.fmin_powell(logL, x, full_output=True, disp=False, xtol=1e-8)
+    if ret[5] > 0:
+        print('Unable to converge')
+        return False
+    x = ret[0]
 
-        start = time.clock()
-        for i in range(0,100):
-            opt.fmin_powell(logL, x, disp=False)
-        end = time.clock()
-        print('Powell: {t:.2f} seconds.'.format(t=end-start))
-
-        start = time.clock()
-        for i in range(0,100):
-            opt.fmin_cg(logL, x, fprime=DlogL, disp=False)
-        end = time.clock()
-        print('CG: {t:.2f} seconds.'.format(t=end-start))
-
-        start = time.clock()
-        for i in range(0,100):
-            opt.fmin_bfgs(logL, x, fprime=DlogL, disp=False)
-        end = time.clock()
-        print('BFGS: {t:.2f} seconds.'.format(t=end-start))
-
-        start = time.clock()
-        for i in range(0,100):
-            opt.fmin_ncg(logL, x, fprime=DlogL, fhess=D2logL, disp=False)
-        end = time.clock()
-        print('Newton-CG: {t:.2f} seconds.'.format(t=end-start))
-
-    x = opt.fmin_bfgs(logL, x, fprime=DlogL, disp=True)
     print(x)
-    print(logL(x))
-    print(D2logL(x))
-    print(linalg.det(D2logL(x)))
-    print(linalg.inv(D2logL(x)))
+    M = D2logL(x)
+    print(M)
 
 p = Player()
 p.rating = 0
@@ -122,20 +101,20 @@ q.cat_dev = [1, 1, 1]
 q.cat = 0
 opps += [q]
 
-q = Player()
-q.rating = 0
-q.cat_rating = [0, 0, 0]
-q.dev = 1
-q.cat_dev = [1, 1, 1]
-q.cat = 1
-opps += [q]
+#q = Player()
+#q.rating = 0
+#q.cat_rating = [0, 0, 0]
+#q.dev = 1
+#q.cat_dev = [1, 1, 1]
+#q.cat = 1
+#opps += [q]
 
-q = Player()
-q.rating = 0
-q.cat_rating = [0, 0, 0]
-q.dev = 1
-q.cat_dev = [1, 1, 1]
-q.cat = 2
-opps += [q]
+#q = Player()
+#q.rating = 0
+#q.cat_rating = [0, 0, 0]
+#q.dev = 1
+#q.cat_dev = [1, 1, 1]
+#q.cat = 2
+#opps += [q]
 
-update(p, opps, [3, 3, 1], [1, 3, 3])
+update(p, opps, [1, 3, 1], [1, 3, 3])
